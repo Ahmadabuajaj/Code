559cdb19089c23c5733c62264fc6dacb07bc78b1
Ahmad Abu-Ajaj <48599024+Ahmadabuajaj@users.noreply.github.com>
2022-11-07 16:54:58 +0200

    Update code5-1.py

--- code5-1.py
+++ code5-1.py
@@ -7,7 +7,10 @@ class Design:
     nodes = []
     
     Edges = []
-    FitnessValues =[6]
+    FitnessValues = []
+    for i in range(6):
+        
+        FitnessValues.append(i)
     #numberOfNodes = int(input("enter"))
     
     def __init__(self,numberOfNodes):
@@ -26,7 +29,7 @@ class Design:
                 self.Edges[2][edge] = random.randrange(1, 6)
                 self.Edges[3][edge] = random.randrange(1, 6)
                 self.Edges[4][edge] = random.randrange(1, 6)
-                self.Edges[5][edge] = random.randrange(1, 5)
+                self.Edges[5][edge] = random.randrange(1, 6)
        
         #print([requirement])
     
@@ -34,8 +37,28 @@ class Design:
     
     
     def fitnessValu(self):
-        for requirement in range(5):
-            self.FitnessValues[requirement] = 5 # eqation of speed 
+        
+        #for requirement in range(6):
+          #  self.FitnessValues[requirement] = 5 # eqation of speed
+            BW =  int(input("Enter Bandwidth:"))
+            DY = int(input("Enter Delay:"))
+            SP = int(input("Enter Speed:"))
+            SY =  int(input("Enter Security:"))
+       
+            self.FitnessValues[0] =  DY # eqation of Delay 
+            self.FitnessValues[1] = 5
+            self.FitnessValues[2] = SY
+            self.FitnessValues[3] =  ((self.numberOfNodes * BW)/60) # th
+            self.FitnessValues[4] = BW * self.numberOfNodes # speed
+            self.FitnessValues[5] =   BW / (BW + DY)# eqation of ava 
+            for i in range(6):
+             print("f",self.FitnessValues[i])
+            
+        
+        
+            
+            
+            
     def print(self):
         #print( (self.numberOfEdges) )
         
@@ -44,7 +67,7 @@ class Design:
 #                print( "d;,;",(requirement) )
                
                  print(self.Edges[requirement][edge])
-                
+               
         """
              def print(self):
                  for requirement in range(5):
@@ -63,8 +86,12 @@ Designs = []
 for d in range(1000):
     design = Design(5)
     Designs.append(design)
-  
+    
 
-Designs[3].print()
+
 print("--------------")
 #Designs[100].print()
+Designs[3].fitnessValu()
+  
+
+#Designs[3].print()
